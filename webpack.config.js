a51dfa777868006d9f10699c3b1c8d253c58852f
vitorcosta039 <vitorcosta588@gmail.com>
2022-02-17 12:22:27 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    filename: "./js/app.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader']
+      // },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { importLoaders: 2 } },
+          { loader: "postcss-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./style/styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "public/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "faq.html",
+      template: "public/faq.html",
+    }),
+  ],
+};
